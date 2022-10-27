c237e9874fcab115f7581b260625f90543b8c48e
Lephenixnoir <sebastien.michelland@protonmail.com>
2022-10-27 17:39:21 +0200

    ports/sh: clean mpconfigport.h and enable modules

--- ports/sh/main.c
+++ ports/sh/main.c
@@ -112,8 +112,3 @@ mp_obj_t mp_builtin_open(size_t n_args, const mp_obj_t *args, mp_map_t *kwargs) 
     return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_KW(mp_builtin_open_obj, 1, mp_builtin_open);
-
-// There is no filesystem so opening a file raises an exception.
-mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
-    mp_raise_OSError(MP_ENOENT);
-}
--- ports/sh/mpconfigport.h
+++ ports/sh/mpconfigport.h
@@ -1,80 +1,71 @@
 #include <stdint.h>
-
-#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_CORE_FEATURES)
-// Other options: BASIC_FEATURES, EXTRA_FEATURES, FULL_FEATURES, EVERYTHING
-
-#define MICROPY_ENABLE_COMPILER     (1)
-#define MP_ENDIANNESS_BIG (1)
-
-#define MICROPY_QSTR_BYTES_IN_HASH  (1)
-// #define MICROPY_QSTR_EXTRA_POOL     mp_qstr_frozen_const_pool
-#define MICROPY_ALLOC_PATH_MAX      (256)
-#define MICROPY_ALLOC_PARSE_CHUNK_INIT (16)
-#define MICROPY_EMIT_X64            (0)
-#define MICROPY_EMIT_THUMB          (0)
-#define MICROPY_EMIT_INLINE_THUMB   (0)
-#define MICROPY_COMP_MODULE_CONST   (0)
-#define MICROPY_COMP_CONST          (0)
-#define MICROPY_COMP_DOUBLE_TUPLE_ASSIGN (0)
-#define MICROPY_COMP_TRIPLE_TUPLE_ASSIGN (0)
-#define MICROPY_MEM_STATS           (0)
-#define MICROPY_DEBUG_PRINTERS      (0)
-#define MICROPY_ENABLE_GC           (1)
-#define MICROPY_GC_ALLOC_THRESHOLD  (0)
-#define MICROPY_REPL_EVENT_DRIVEN   (0)
-#define MICROPY_HELPER_REPL         (1)
-#define MICROPY_HELPER_LEXER_UNIX   (0)
-#define MICROPY_ENABLE_SOURCE_LINE  (1)
-#define MICROPY_ENABLE_DOC_STRING   (0)
-#define MICROPY_ERROR_REPORTING     (MICROPY_ERROR_REPORTING_DETAILED)
-#define MICROPY_BUILTIN_METHOD_CHECK_SELF_ARG (1)
-#define MICROPY_PY_ASYNC_AWAIT      (0)
-#define MICROPY_PY_BUILTINS_BYTEARRAY (1)
-#define MICROPY_PY_BUILTINS_MEMORYVIEW (1)
-#define MICROPY_PY_BUILTINS_ENUMERATE (1)
-#define MICROPY_PY_BUILTINS_FILTER  (1)
-// #define MICROPY_PY_BUILTINS_FROZENSET (1)
-#define MICROPY_PY_BUILTINS_REVERSED (1)
-#define MICROPY_PY_BUILTINS_SET     (1)
-#define MICROPY_PY_BUILTINS_SLICE   (1)
-#define MICROPY_PY_BUILTINS_PROPERTY (1)
-#define MICROPY_PY_BUILTINS_MIN_MAX (1)
-#define MICROPY_PY___FILE__         (0)
-#define MICROPY_PY_GC               (1)
-#define MICROPY_PY_ARRAY            (1)
-#define MICROPY_PY_ATTRTUPLE        (1)
-#define MICROPY_PY_COLLECTIONS      (1)
-#define MICROPY_PY_MATH             (1)
-#define MICROPY_PY_CMATH            (1)
-#define MICROPY_PY_IO               (1)
-#define MICROPY_PY_STRUCT           (1)
-#define MICROPY_PY_SYS              (1)
-// #define MICROPY_MODULE_FROZEN_MPY   (1)
-#define MICROPY_CPYTHON_COMPAT      (0)
-#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_MPZ)
-#define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_DOUBLE)
-// #define MICROPY_PY_URANDOM (1)
-#define MICROPY_PY_URANDOM_EXTRA_FUNCS (1)
-#define MP_NEED_LOG2 (1)
-
-/* #define MP_PLAT_PRINT_STRN(str, len) mp_hal_stdout_tx_strn_cooked(str, len)
-
-// extra built in names to add to the global namespace
-#define MICROPY_PORT_BUILTINS \
-    { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) },
-
-#define MICROPY_MIN_USE_STDOUT (1) */
-
-// Type definitions for the specific machine.
-
-typedef intptr_t mp_int_t; // must be pointer size
-typedef uintptr_t mp_uint_t; // must be pointer size
-typedef long mp_off_t;
-
-// We need to provide a declaration/definition of alloca().
 #include <alloca.h>
 
-// Define the port's name and hardware.
+/* General feature set selection
+   Other options: BASIC_FEATURES, EXTRA_FEATURES, FULL_FEATURES, EVERYTHING */
+#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_CORE_FEATURES)
+
+/* Main features */
+#define MICROPY_ENABLE_COMPILER           (1)
+#define MICROPY_ENABLE_GC                 (1)
+#define MP_ENDIANNESS_BIG                 (1)
+#define MICROPY_READER_POSIX              (1)
+#define MICROPY_ERROR_REPORTING           (MICROPY_ERROR_REPORTING_DETAILED)
+#define MICROPY_LONGINT_IMPL              (MICROPY_LONGINT_IMPL_MPZ)
+#define MICROPY_FLOAT_IMPL                (MICROPY_FLOAT_IMPL_DOUBLE)
+
+/* Other features that we select against MICROPY_CONFIG_ROM_LEVEL */
+#define MICROPY_PY_FSTRINGS               (1) /* in EXTRA_FEATURES */
+#define MICROPY_HELPER_REPL               (1) /* in EXTRA_FEATURES */
+#define MICROPY_ENABLE_SOURCE_LINE        (1) /* in EXTRA_FEATURES */
+#define MICROPY_PY_BUILTINS_STR_UNICODE   (1) /* in EXTRA_FEATURES */
+#define MICROPY_PY_BUILTINS_HELP_MODULES  (1) /* in EXTRA_FEATURES */
+// #define MICROPY_PY_SYS_STDFILES          (1) /* in EXTRA_FEATURES */
+
+#define MICROPY_ALLOC_PATH_MAX            (256)
+#define MICROPY_ALLOC_PARSE_CHUNK_INIT    (32)
+#define MICROPY_MEM_STATS                 (0)
+#define MICROPY_GC_ALLOC_THRESHOLD        (1)
+#define MICROPY_ENABLE_DOC_STRING         (0)
+#define MICROPY_BUILTIN_METHOD_CHECK_SELF_ARG (1)
+
+#define MICROPY_PY_BUILTINS_BYTEARRAY     (1)
+#define MICROPY_PY_BUILTINS_ENUMERATE     (1)
+#define MICROPY_PY_BUILTINS_FILTER        (1)
+#define MICROPY_PY_BUILTINS_FROZENSET     (1)
+#define MICROPY_PY_BUILTINS_HELP          (1)
+#define MICROPY_PY_BUILTINS_INPUT         (1)
+#define MICROPY_PY_BUILTINS_MEMORYVIEW    (1)
+#define MICROPY_PY_BUILTINS_MIN_MAX       (1)
+#define MICROPY_PY_BUILTINS_PROPERTY      (1)
+#define MICROPY_PY_BUILTINS_REVERSED      (1)
+#define MICROPY_PY_BUILTINS_SET           (1)
+#define MICROPY_PY_BUILTINS_SLICE         (1)
+
+/* Extra built-in modules */
+#define MICROPY_PY_ARRAY                  (1)
+#define MICROPY_PY_COLLECTIONS            (1)
+#define MICROPY_PY_MATH                   (1)
+#define MICROPY_PY_CMATH                  (1)
+#define MICROPY_PY_GC                     (1)
+#define MICROPY_PY_IO                     (1)
+#define MICROPY_PY_STRUCT                 (1)
+#define MICROPY_PY_SYS                    (1)
+#define MICROPY_PY_URANDOM                (1)
+#define MICROPY_PY_URANDOM_EXTRA_FUNCS    (1)
+
+/* Enable alias of u-modules, eg. urandom -> random */
+#define MICROPY_MODULE_WEAK_LINKS         (1)
+
+/* extra built in names to add to the global namespace
+#define MICROPY_PORT_BUILTINS \
+    { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) }, */
+
+/* Machine-specific type definitions */
+typedef intptr_t mp_int_t;
+typedef uintptr_t mp_uint_t;
+typedef long mp_off_t;
+
 #define MICROPY_HW_BOARD_NAME "sh7305"
 #define MICROPY_HW_MCU_NAME   "sh-4a"
 
