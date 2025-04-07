8fa48bb98614cb57d4e7a82cbe03ed040a5b64ac
Sylvain PILLOT <pillot.sylvian@gmail.com>
2025-04-07 08:51:24 +0200

    Added some examples with --py-compact option in the fxconv command line

--- ports/sh/examples/font-support/fonttest1.py
+++ ports/sh/examples/font-support/fonttest1.py
@@ -3,9 +3,11 @@ from gint import *
 
 dclear(C_WHITE)
 
+dtext(10,10,C_BLUE,"Hello before changing font")
+
 print("TEST : Shmup font (Proportional)")
 dfont(font_shmup)
-dtext(10,10,C_RED,"Hello")
+dtext(10,50,C_RED,"Hello after changing font")
 
 dupdate()
 getkey()
--- ports/sh/examples/font-support/fonttest2.py
+++ ports/sh/examples/font-support/fonttest2.py
@@ -3,9 +3,11 @@ from gint import *
 
 dclear(C_WHITE)
 
-print("TEST : Numworks font (Monospaced)")
+dtext(10,10,C_BLUE,"Hello before changing font")
+
+print("TEST : Shmup font (Proportional)")
 dfont(font_numworks)
-dtext(10,50,C_BLUE,"Hello")
+dtext(10,50,C_RED,"Hello after changing font")
 
 dupdate()
 getkey()
